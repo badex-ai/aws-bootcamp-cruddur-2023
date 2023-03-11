f6845a116f3c936de34433f48fe73132164adbd0
Badamasi Aliu <Badamasi.link@gmail.com>
2023-03-11 00:26:26 +0000

    added files for honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,8 +14,16 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 # Honeycomb --------
-# Initalize tracing and exorter that can send data to Honeycomb
+# Initalize tracing and exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
